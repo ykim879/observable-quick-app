984e874331b62b4508b51d65cb966c4376466c29
ykim8 <ykim879@gatech.edu>
2023-05-08 10:08:34 -0700

    first set up

---
+++ order-users-relation/src/app/home/home.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+//create two observables, and one sitemap, user observable subscrbie to orderid and orderid subscribe to userid
+  constructor() {}
+
+}
